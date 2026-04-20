4fdc055381d4b6913b129fad97939a47519c8756
CINY <ciy5364612@gmail.com>
2026-04-21 00:50:17 +0900

    [#2] main 수정 및 war 빌드 방식 추가

--- spring/studyzip/src/main/java/com/example/Main.java
+++ spring/studyzip/src/main/java/com/example/Main.java
@@ -1,14 +1,6 @@
 package com.example;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 public class Main {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(WebConfig.class);
-
-        ExampleController exampleService = context.getBean(ExampleController.class);
-        System.out.println(exampleService.sayHello());
-
-        context.close();
     }
 }
