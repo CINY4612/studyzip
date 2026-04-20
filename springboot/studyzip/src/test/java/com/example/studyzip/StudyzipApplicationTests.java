1079c5d7c3e1209a1ac834e06401519a32911c23
CINY <ciy5364612@gmail.com>
2026-04-20 23:49:14 +0900

    [#2] SpringBoot 프로젝트 초기화

---
+++ springboot/studyzip/src/test/java/com/example/studyzip/StudyzipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyzipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
