b805d0948e6b1d5194155bc66ed5e5d187d57f4f
CINY <ciy5364612@gmail.com>
2026-05-20 18:19:54 +0900

    [#7] users API 요청 시 동작 추가 실습

--- nodejs/express-study/routes/users.js
+++ nodejs/express-study/routes/users.js
@@ -1,14 +1,45 @@
 /**
  * app.js 의 app.use('users', usersRouter) 와 연결되어
- * GEt /users 요청을 처리함
+ * GET /users 요청을 처리함
  */
 
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var users = [
+  { id: 1, name: 'Zoey' },
+  { id: 2, name: 'Minako' }
+];
+
+router.get('/', function(req, res){
+  res.json(users);
+});
+
+router.get('/:id', function(req, res) {
+  var id = Number(req.params.id);
+  var user = users.find(function(user) {
+    return user.id === id;
+  });
+
+  if (!user) {
+    return res.status(404).json({
+      message: 'User not found'
+    });
+  }
+
+  res.json(user);
+})
+
+router.post('/', function(req, res){
+  var name = req.body.name;
+
+  var newUser = {
+    id: users.length + 1,
+    name: name
+  };
+
+  users.push(newUser);
+  res.status(201).json(newUser);
 });
 
 module.exports = router;
